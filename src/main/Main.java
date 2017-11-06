d77aedde1233e09ed5cfa1882201bc5920c3a9cb
hendrik185 <hendrik@jan-bert.nl>
2017-11-06 04:47:36 +0100

    //testing if this push auto runs jenkins

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,6 @@
 public class Main {
-
+    //testing if this push auto runs jenkins
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
-//awdawdwadaw
